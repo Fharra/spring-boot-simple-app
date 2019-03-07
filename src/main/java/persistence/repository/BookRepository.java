b0648ea02edfd38ce37df288db43932f784b5c11
Fharra <fharra1@hotmail.com>
2019-03-07 12:16:56 +0100

    creates repository and entity

---
+++ src/main/java/persistence/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package persistence.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import persistence.model.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+	List<Book> findByTitle(String title);
+}
--- src/main/java/simpleApp/Application.java
+++ src/main/java/simpleApp/Application.java
@@ -2,7 +2,12 @@ package simpleApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+// scan the specified package for repositories
+@EnableJpaRepositories("persistence.repository")
+@EntityScan("persistence.model") // to pick up JPA entities
 @SpringBootApplication
 public class Application {
 
