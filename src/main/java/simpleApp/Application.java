ec2d3f0a81b4d346298cacb98b51f582fe117181
Fharra <fharra1@hotmail.com>
2019-03-07 17:21:05 +0100

    adds tests failing

--- src/main/java/simpleApp/Application.java
+++ src/main/java/simpleApp/Application.java
@@ -1,19 +1,10 @@
 package simpleApp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// scan the specified package for repositories
-@EnableJpaRepositories("persistence.repository")
-@EntityScan("persistence.model") // to pick up JPA entities
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
 	}
-
 }
